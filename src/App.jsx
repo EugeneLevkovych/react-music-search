a92195783207cb471b54a20c82db15d7a14f79e0
Eugene <eugenelevkovych@yahoo.com>
2025-04-30 13:38:53 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Search from "./components/Search";
+import Header from "./components/Header";
+import Result from "./components/Result";
+import "./scss/main.scss";
+
 export default function App() {
-  
+  return (<>
+  <Header />
+  <Search />
+  <Result />
+  </>)
 }
 
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Search() {
+  const [query, setQuery] = useState("");
+
+    return ( <input
+        className="search"
+        type="text"
+        placeholder="Search music..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />)
+}
